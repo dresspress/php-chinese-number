913c64aa471a98826b41c96d3246a62babab1503
Cloud <cloudinstone@gmail.com>
2024-04-25 20:07:32 +0800

    dev

--- src/ChineseNumberHelper.php
+++ src/ChineseNumberHelper.php
@@ -76,22 +76,22 @@ class ChineseNumberHelper {
         $num = 0;
         $wan = explode('万', $string);
         if (count($wan) > 1) {
-            $num += self::toNumber1($wan[0]) * 10000;
+            $num += self::toNumber($wan[0]) * 10000;
             $string = $wan[1];
         }
         $qian = explode('千', $string);
         if (count($qian) > 1) {
-            $num += self::toNumber1($qian[0]) * 1000;
+            $num += self::toNumber($qian[0]) * 1000;
             $string = $qian[1];
         }
         $bai = explode('百', $string);
         if (count($bai) > 1) {
-            $num += self::toNumber1($bai[0]) * 100;
+            $num += self::toNumber($bai[0]) * 100;
             $string = $bai[1];
         }
         $shi = explode('十', $string);
         if (count($shi) > 1) {
-            $num += self::toNumber1($shi[0] ? $shi[0] : '一') * 10;
+            $num += self::toNumber($shi[0] ? $shi[0] : '一') * 10;
             $string = $shi[1] ? $shi[1] : '零';
         }
         $ling = explode('零', $string);
